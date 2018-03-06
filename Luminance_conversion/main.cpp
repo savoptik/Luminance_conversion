29e15e72de62e73219dd662296a01b8390136b1e
savoptik <sav70792@gmail.com>
2018-03-06 12:34:46 +0300

    Максимум изменён на среднее.

--- Luminance_conversion/main.cpp
+++ Luminance_conversion/main.cpp
@@ -11,7 +11,6 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
-#include <cmath>
 using namespace cv;
 using namespace std;
 
@@ -23,23 +22,28 @@ int main(int argc, const char * argv[]) {
     int g = 0; // максимум зелёных.
     int b = 0; // максимум синих.
     int max = 0; // максимум.
+    int colp = img.rows * img.cols;
     // Поиск максимумов в каналах.
     for (int i = 0; i < img.rows; i++) {
         for (int j = 0; j < img.cols; j++) {
-            b = img.at<Vec3b>(i, j)[0] > b ? img.at<Vec3b>(i, j)[0]: b;
-            g = img.at<Vec3b>(i, j)[1] > g ? img.at<Vec3b>(i, j)[1]: g;
-            r = img.at<Vec3b>(i, j)[2] > r ? img.at<Vec3b>(i, j)[2]: r;
+            b += img.at<Vec3b>(i, j)[0];
+            g += img.at<Vec3b>(i, j)[1];
+            r += img.at<Vec3b>(i, j)[2];
         }
     }
+    b = b /colp;
+    g = g / colp;
+    r = r / colp;
     int midMax = (r + g + b)/3; // среднее максимумов.
     // Преобразование.
     for (int i = 0; i < img.rows; i++) {
         for (int j = 0; j < img.cols; j++) {
-            img.at<Vec3b>(i, j)[0] = img.at<Vec3b>(i, j)[0] * midMax / b; // преобразование канала.
+            auto buf = img.at<Vec3b>(i, j);
+            img.at<Vec3b>(i, j)[0] = buf[0] * midMax / b; // преобразование канала.
             max = img.at<Vec3b>(i, j)[0] > max ? img.at<Vec3b>(i, j)[0]: max; // поиск максимума.
-            img.at<Vec3b>(i, j)[1] = img.at<Vec3b>(i, j)[1] * midMax / g;
+            img.at<Vec3b>(i, j)[1] = buf[1] * midMax / g;
             max = img.at<Vec3b>(i, j)[1] > max ? img.at<Vec3b>(i, j)[1]: max;
-            img.at<Vec3b>(i, j)[2] = img.at<Vec3b>(i, j)[2] * midMax / r;
+            img.at<Vec3b>(i, j)[2] = buf[2] * midMax / r;
             max = img.at<Vec3b>(i, j)[2] > max ? img.at<Vec3b>(i, j)[2]: max;
         }
     }
